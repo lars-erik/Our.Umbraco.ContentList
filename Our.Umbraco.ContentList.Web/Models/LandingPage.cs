a4c274eb2a3cedc97d2717c8b44e43e624376cd6
Bianca Eilertsen <bianca@markedspartner.no>
2020-02-07 15:14:11 +0100

    Moved fiels from AppData/Models to Models folder

---
+++ Our.Umbraco.ContentList.Web/Models/LandingPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using Our.Umbraco.ContentList;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Web.PublishedContentModels
+{
+
+    public partial class LandingPage : IListableContent
+    {
+        public IHtmlString ListHeading => new HtmlString(this.Name);
+        public string ListImageUrl => string.Empty;
+        public IHtmlString ListSummary => new HtmlString("her finner du et summary");
+        public IHtmlString ReadMoreText => new HtmlString("les mer");
+    }
+}
