a1680f1fea78ad2f2b31100ead63b8024e656c48
Lars-Erik Aabech <lea@markedspartner.no>
2021-10-19 16:52:47 +0200

    Controllers created w/o content. Need to find new nice way to register metadata.

---
+++ src/Our.Umbraco.ContentList/Controllers/ContentListApiController.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Cms.Web.BackOffice.Controllers;
+using Umbraco.Cms.Web.Common.Attributes;
+
+namespace Our.Umbraco.ContentList.Controllers
+{
+    [PluginController("OurContentList")]
+    public class ContentListApiController : UmbracoAuthorizedApiController
+    {
+        public ContentListApiController()
+        {
+        }
+    }
+}
